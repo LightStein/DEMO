c3273613cae6cd40ade68e16a1a0eac8f7b10621
Thomas <kakhayavelashvili@hotmail.com>
2020-08-20 17:21:10 -0700

    Add Headless chrome support

--- src/main/java/com/github/automatedowl/examples/drivers/DriverModule.java
+++ src/main/java/com/github/automatedowl/examples/drivers/DriverModule.java
@@ -1,9 +1,11 @@
 package com.github.automatedowl.examples.drivers;
 
 import com.google.inject.AbstractModule;
+import io.github.bonigarcia.wdm.ChromeDriverManager;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 /**
  * Guice configuration module for WebDriver object.
@@ -12,12 +14,14 @@ public class DriverModule extends AbstractModule {
 
     /** Configure method. It binds a selected type of BrowserDriver. */
     protected synchronized void configure() {
-
+        ChromeDriverManager.chromedriver().setup();
         // Set Chromedriver executable path.
         setChromedriverPath();
 
+        ChromeOptions chromeoptions = new ChromeOptions();
+        chromeoptions.addArguments("--headless");
         // Bind WebDriver object to ChromeDriver.
-        bind(WebDriver.class).toInstance(new ChromeDriver());
+        bind(WebDriver.class).toInstance(new ChromeDriver(chromeoptions));
     }
 
     private void setChromedriverPath() {
